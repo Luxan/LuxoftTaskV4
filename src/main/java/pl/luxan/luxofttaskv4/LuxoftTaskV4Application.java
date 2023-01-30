ba272788d1b9741fc3086a29617ad2937ab9712c
Sergey Gorokh <luxawes@gmail.com>
2023-01-31 00:36:15 +0100

    Configured Hybernate manually and excluded HibernateJpaAutoConfiguration to eliminate intelliJ Idea syntax error.

--- src/main/java/pl/luxan/luxofttaskv4/LuxoftTaskV4Application.java
+++ src/main/java/pl/luxan/luxofttaskv4/LuxoftTaskV4Application.java
@@ -2,9 +2,10 @@ package pl.luxan.luxofttaskv4;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = HibernateJpaAutoConfiguration.class)
 @EnableJpaRepositories(basePackages="pl.luxan.luxofttaskv4")
 public class LuxoftTaskV4Application {
 
